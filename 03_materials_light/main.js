c64fc347292ccbcc0c1b4e2a109eca6b301f6426
krapnik <krapnik@qq.com>
2020-09-02 08:38:41 +0800

    update

--- 03_materials_light/main.js
+++ 03_materials_light/main.js
@@ -6,6 +6,7 @@ function init() {
     var renderer = new THREE.WebGLRenderer();
     renderer.setClearColor(new THREE.Color(0x000000));
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;//开启阴影效果
 
     //创建坐标轴对象，轴线设置粗细值20
     var axes = new THREE.AxesHelper(20);
@@ -17,30 +18,37 @@ function init() {
     var plane = new THREE.Mesh(planeGeometry, planeMaterial);//网格对象
     plane.rotation.x = -0.5 * Math.PI;
     plane.position.set(15, 0, 0);
+    plane.receiveShadow = true;
     scene.add(plane);
 
-    var cubeGeometry = new THREE.BoxGeometry(4,4,4);
-    var cubeMaterial = new THREE.MeshLambertMaterial({color:0XFF0000,});
-    var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
-    cube.position.set(-4,3,0);
+    var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+    var cubeMaterial = new THREE.MeshLambertMaterial({ color: 0XFF0000, });
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(-4, 3, 0);
+    cube.castShadow = true;
     scene.add(cube);
 
-    var sphereGeometry = new THREE.SphereGeometry(4,20,20);
-    var sphereMaterial = new THREE.MeshLambertMaterial({color:0x7777ff});
-    var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
-    sphere.position.set(20,4,2);
+    var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+    var sphereMaterial = new THREE.MeshLambertMaterial({ color: 0x7777ff });
+    var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    sphere.position.set(20, 4, 2);
+    sphere.castShadow = true;
     scene.add(sphere);
 
-    camera.position.set(-30,40,30);
+    camera.position.set(-30, 40, 30);
     camera.lookAt(scene.position);
 
     var spotLight = new THREE.SpotLight(0xFFFFFF);
-    spotLight.position.set(-40,40,-15);
+    spotLight.position.set(-40, 40, -15);
     spotLight.castShadow = true;//开启阴影功能
-    spotLight.shadow.mapSize = new THREE.Vector2(1024,1024);
+    spotLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
     spotLight.shadow.camera.far = 130;
     spotLight.shadow.camera.near = 40;
+    scene.add(spotLight);
+
+    var ambienLight = new THREE.AmbientLight(0x353535);//环境光线
+    scene.add(ambienLight);
 
     document.querySelector("#webgl-output").appendChild(renderer.domElement);
-    renderer.render(scene,camera);
+    renderer.render(scene, camera);
 }
